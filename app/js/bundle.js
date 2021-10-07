71d753da78269e0ea4f7c9b2de7ac406e66833f0
Andrew <andrew.ndrw1@gmail.com>
2021-10-08 00:25:57 +0300

    bucket

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -135,11 +135,13 @@ const burger = () => {
 __webpack_require__.r(__webpack_exports__);
 const filterMenu = () => {
   const cardsCenter = document.querySelector('.menu__inner');
-  const cardsAmount = document.querySelector('.header__cart-footer__amount');
-  const cardsTotal = document.querySelector('.header__cart-footer__total');
+  let cardsAmount = document.querySelector('.amount');
+  let cardsTotal = document.querySelector('.total');
   const cartContent = document.querySelector('.header__cart-wrapper');
   const cartOverlay = document.querySelector('.header__cart-overlay');
-  const closeCard = document.querySelector('.header__cart-close');
+  const footerReset = document.querySelector('.footer__reset');
+  const closeCardBtn = document.querySelector('.header__cart-close');
+  const cartBtn = document.querySelector('.header__cart');
   let cart = [];
   let buttonsDOM = [];
 
@@ -180,19 +182,19 @@ const filterMenu = () => {
       let result = '';
       products.forEach(product => {
         result += `
-        <div class="menu__item">
-        <img src=${product.image} alt="menu" class="menu__img">
-    <div class="menu__content">
-            <div class="menu__subtitle">${product.title}</div>
-            <div class="menu__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro architecto reprehenderit</div>
-        <div class="menu__info">
-        <div class="menu__amount">Price</div>
-            <div class="menu__price">${product.price}$</div>
-        </div>
-            <button type="button" class="menu__addtocart" data-id=${product.id}>Add to cart</button>
-        </div>
-     </div>
-        `;
+            <div class="menu__item">
+            <img src=${product.image} alt="menu" class="menu__img">
+        <div class="menu__content">
+                <div class="menu__subtitle">${product.title}</div>
+                <div class="menu__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro architecto reprehenderit</div>
+            <div class="menu__info">
+            <div class="menu__amount">Price</div>
+                <div class="menu__price">${product.price}$</div>
+            </div>
+                <button type="button" class="menu__addtocart" data-id=${product.id}>Add to cart</button>
+            </div>
+         </div>
+            `;
       });
       cardsCenter.innerHTML = result;
     } // filterProducts(products) {
@@ -228,49 +230,62 @@ const filterMenu = () => {
           console.log(cart); //добавить в локальное хранилище
 
           Storage.saveCart(cart); //установить значение карточек
-          // this.setCArtValues(cart)
-          //отобразить карточку
+
+          this.setCartValues(cart); //отобразить карточку
 
           this.addCartItem(cartItem); //показать карточку
 
           this.showCart();
         });
       });
-    } // setCArtValues(cart) {
-    //     let tempTotal = 0
-    //     let itemsTotal = 0
-    //     cart.map(item => {
-    //         tempTotal += item.price * item.amount
-    //         itemsTotal += item.amount
-    //     })
-    //     cardsTotal.innerText = parseFloat(tempTotal.toFixed(2))
-    //     cardsAmount.innerText = itemsTotal
-    // }
+    }
 
+    setCartValues(cart) {
+      let tempTotal = 0;
+      let itemsTotal = 0;
+      cart.map(item => {
+        tempTotal += item.price * item.amount;
+        itemsTotal += item.amount;
+      });
+      cardsTotal.innerText = parseFloat(tempTotal.toFixed(2));
+      cardsAmount.innerText = itemsTotal;
+    }
 
     addCartItem(item) {
       const div = document.createElement('div');
       div.classList.add('header__cart-content');
       div.innerHTML = `
-<img src=${item.image} alt="product" class="header__cart-image">
-<div class="header__cart-info">
-    <div class="header__cart-flex">
-    <div class="header__cart-title">${item.title}</div>
-    <div class="header__cart-price">${item.price} $</div>
-</div>
-    <div class="header__cart-footer__content">
-        <div class="header__cart-footer__amount">Amount: <span>${item.amount}</span></div>
-        <div class="header__cart-footer__total">Total: <span>5.75$</span></div>
+    <img src=${item.image} alt="product" class="header__cart-image">
+    <div class="header__cart-info">
+        <div class="header__cart-flex">
+        <div class="header__cart-title">${item.title}</div>
+        <div class="header__cart-price">${item.price} $</div>
     </div>
-</div>
-<img src="images/cart/close.svg" alt="close" class="header__cart-close">
-<img src="images/cart/delete.svg" alt="bucket" class="header__cart-remove" data-id=${item.id}>
-`;
+    </div>
+    <img src="images/cart/close.svg" alt="close" class="header__cart-close">
+    <img src="images/cart/delete.svg" alt="bucket" class="header__cart-remove">
+    `;
       cartContent.appendChild(div);
     }
 
     showCart() {
       cartOverlay.classList.add('showCart');
+    }
+
+    setupAPP() {
+      cart = Storage.getCart();
+      this.setCartValues();
+      this.populateCart(cart);
+      cartBtn.addEventListener('click', this.showCart);
+      closeCardBtn.addEventListener('click', this.hideCart);
+    }
+
+    populateCart(cart) {
+      cart.forEach(item => this.addCartItem(item));
+    }
+
+    hideCart() {
+      cartOverlay.classList.remove('showCart');
     }
 
   }
@@ -289,10 +304,16 @@ const filterMenu = () => {
       localStorage.setItem('cart', JSON.stringify(cart));
     }
 
+    static getCart() {
+      return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+    }
+
   }
 
   const ui = new UI();
-  const products = new Products();
+  const products = new Products(); // настройка приложения
+  // ui.setupAPP()
+
   products.getProducts().then(products => {
     ui.displayProducts(products);
     Storage.saveProducts(products);
@@ -301,40 +322,7 @@ const filterMenu = () => {
   });
 };
 
-/* harmony default export */ __webpack_exports__["default"] = (filterMenu); // filterBtns.forEach(btn => {
-// btn.addEventListener('click', (e) => {
-//     const category = e.currentTarget.dataset.id
-//     const menuCategory = items.filter(function(menuItem) {
-//         if (menuItem.category === category) {
-//             return menuItem
-//         }
-//     })
-//     if (category === 'all') {
-//         displayMenuItems(items)
-//     } else {
-//         displayMenuItems(menuCategory)
-//     }
-// })
-// })
-// function displayMenuItems(menuItems) {
-//     let displaymenu = menuItems.map(function(elem) {
-//         return `
-//         <div class="menu__item">
-//         <img src=${elem.fields.image} alt="menu" class="menu__img">
-//         <div class="menu__content">
-//             <div class="menu__subtitle">${elem.fields.title}</div>
-//             <div class="menu__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro architecto reprehenderit</div>
-//             <div class="menu__info">
-//             <div class="menu__amount">Price</div>
-//             <div class="menu__price">${elem.fields.price}$</div>
-//         </div>
-//             <button type="button" class="menu__addtocart">Add to cart</button>
-//         </div>
-//     </div>
-//         `
-//     }).join("")
-//     cardsCenter.innerHTML = displaymenu
-// }
+/* harmony default export */ __webpack_exports__["default"] = (filterMenu);
 
 /***/ }),
 
